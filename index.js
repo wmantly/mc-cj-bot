efd042f202d8431916da422c43e0ce36cbda3b12
William Mantly <wmantly@gmail.com>
2022-06-25 19:00:40 -0400

    Added Ethan63020 to inv

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ commands = {
     },
     inv:{
         desc: `Have bot invite you to its position`,
-        allowed: ['wmantly', 'useless666', 'VinceNL'],
+        allowed: ['wmantly', 'useless666', 'VinceNL', 'Ethan63020'],
         function(from){
             minecraftBot.chat(`/invite ${from}`)
             unLockCommand(1);
